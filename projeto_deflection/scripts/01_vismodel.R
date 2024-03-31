a7159ab9716b1d727c0ea0fc674f74c3ac889f97
Diogo Silva <56855879+Diogojackson@users.noreply.github.com>
2024-03-31 18:45:42 +1000

    first commit

---
+++ projeto_deflection/scripts/01_vismodel.R
@@ -0,0 +1,21 @@
+# Script para modelagem visual tinta vs real
+# author: Diogo Silva
+# Mon Feb  5 16:06:05 2024 ------------------------------
+# last update:
+# Mon Feb  5 16:06:09 2024 ------------------------------
+
+#Packages ----
+library(tidyverse)
+library(pavo)
+library(colorspec)
+
+#Import data ----
+reflet <- read.csv("data/raw/refletancias/00_refletancias.csv")
+reflet <- fixspec(reflet)
+colnames(reflet)
+
+#Vismodel ----
+#Comparando as tintas com a refletancia natural ----
+vis <- vis.peafowl(reflet, background = "gv_q_Reflection_00002", illum = "D65")
+vis
+

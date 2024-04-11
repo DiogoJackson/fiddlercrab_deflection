9eeec80501d4bf6d7047a5da9b7d1a433b73a8ac
Diogo Silva <56855879+Diogojackson@users.noreply.github.com>
2024-04-11 17:05:05 +1000

    update

--- projeto_deflection/scripts/01_S_aggplot.R
+++ projeto_deflection/scripts/01_S_aggplot.R
@@ -9,52 +9,11 @@
 library(tidyverse)
 library(pavo)
 library(colorspec)
+library(cowplot)
 
 #Import procspec data tintas ----
-reflet <- read.csv("data/raw/refletancias/00_refletancias.csv")
-reflet <- fixspec(reflet)
-
 nat <- read.csv("data/raw/refletancias/00_natural_reflectances.csv")
 nat <- fixspec(nat)
-
-bkg <- read.csv("data/raw/refletancias/00_bkg_reflectances.csv")
-bkg <- fixspec(bkg)
-
-#Comparing paints ----
-
-#Orange UV paint ----
-colnames(reflet)
-
-refletancias <- reflet %>% 
-  rename(`Natural claw` = gv_q_Reflection_00002,
-         `Orange paint` = orange_paint_kaisercraft_pure_Reflection_001,
-         `UV orange paint` = `orange_paint.4g_ba.5g_Reflection_001`)
-
-plotspec(refletancias,
-         ylim = c(0,100),
-         legend = T)
-
-#Naturals ----
-plot(nat, ylim = c(0,100))
-colnames(nat)
-
-nat <- nat %>% 
-  rename("Propodus" = contains("_prop_")) %>% 
-  rename("Carapace" = contains("_cara_")) %>% 
-  rename("Spot" = contains("_caraspot_")) %>% 
-  rename("Dactylus" = contains("_dact_")) %>% 
-  rename("Manus" = contains("_ponta_"))
-
-#Plot ----
-
-plotspec(nat, 
-         legend = TRUE, 
-         ylim = c(0, 100),
-         main = expression(italic("Gelasimus vomeris")))
-
-plotspec(bkg, 
-         legend = TRUE,
-         main = expression(italic("Backgrounds")))
 
 #Comparing claw colors with paints ----
 
@@ -63,24 +22,22 @@ nat <- nat %>%
   rename("Carapace" = contains("_cara_")) %>% 
   rename("Spot" = contains("_caraspot_")) %>% 
   rename("Dactylus" = contains("_dact_")) %>% 
-  rename("Manus" = contains("_ponta_"))
+  rename("Manus" = contains("_ponta_")) %>% 
+  rename("Mudflat" = contains("_areia")) %>% 
+  rename("Robot_manus" = contains("laranja")) %>% 
+  rename("Robot_dactyl" = contains("rosa")) %>% 
+  rename("Cloth" = contains("pano")) %>% 
+  rename("Robot_carapace" = contains("preto"))
+nat
 
 #2.1. Colors comparation (natural vs paint) ----
 
-#To Do list:
-# 1 = adicionar procspec na pasta "nat"
-# 2 = importar spectros
-# 3 = ajustar nome do rename: Manus_paint
-# 4 = ajustar geom_line: ex: Manus2 para Manus_paint.
-
-#Manus ----
-
-vis_manus <- vis.peafowl(nat, background = "Manus1", illum = "D65")
+vis_manus <- vis.peafowl(nat, background = "Manus2", illum = "D65")
 vis_manus
 
 p1 <- ggplot(nat, aes(wl))+
-  geom_line(aes(y = Manus1, linetype = "Natural"), color = "#ffcf00", linewidth = 0.7)+
-  geom_line(aes(y = Manus2, linetype = "Paint"), color = "black", linewidth = 0.7)+
+  geom_line(aes(y = Manus2, linetype = "Natural"), color = "#ffcf00", linewidth = 0.7)+
+  geom_line(aes(y = Robot_manus2, linetype = "Paint"), color = "black", linewidth = 0.7)+
   ylim(0, 100)+
   labs(title = expression(bold("Manus")),
        x = "Wavelength (nm)",
@@ -96,8 +53,8 @@ p1
 
 #Dactylus ----
 p2 <- ggplot(nat, aes(wl))+
-  geom_line(aes(y = Dactylus1, linetype = "Natural"), color = "pink", linewidth = 0.7)+
-  geom_line(aes(y = Dactylus2, linetype = "Paint"), color = "black", linewidth = 0.7)+
+  geom_line(aes(y = Dactylus2, linetype = "Natural"), color = "pink", linewidth = 0.7)+
+  geom_line(aes(y = Robot_dactyl2, linetype = "Paint"), color = "black", linewidth = 0.7)+
   ylim(0, 100)+
   labs(title = expression(bold("Dactylus")),
        x = "Wavelength (nm)",
@@ -114,7 +71,7 @@ p2
 #Carapace ----
 p3 <- ggplot(nat, aes(wl))+
   geom_line(aes(y = Carapace2, linetype = "Natural"), color = "grey", linewidth = 0.7)+
-  geom_line(aes(y = Carapace1, linetype = "Paint"), color = "black", linewidth = 0.7)+
+  geom_line(aes(y = Robot_carapace1, linetype = "Paint"), color = "black", linewidth = 0.7)+
   ylim(0, 100)+
   labs(title = expression(bold("Carapace")),
        x = "Wavelength (nm)",
@@ -128,32 +85,35 @@ p3 <- ggplot(nat, aes(wl))+
   guides(linetype = guide_legend(title = NULL))
 p3
 
+p4 <- ggplot(nat, aes(wl))+
+  geom_line(aes(y = Mudflat2, linetype = "Mudflat"), color = "brown", linewidth = 0.7)+
+  geom_line(aes(y = Cloth1, linetype = "Cloth"), color = "black", linewidth = 0.7)+
+  ylim(0, 100)+
+  labs(title = expression(bold("Carapace")),
+       x = "Wavelength (nm)",
+       y = "Reflectance (%)")+
+  theme_test(base_size = 8)+
+  scale_linetype_manual(values = c("Cloth" = "dashed", "Mudflat" = "solid")) +
+  theme(legend.key.height= unit(0.3, 'cm'),
+        legend.key.width= unit(1, 'cm'),
+        legend.position = c(0.70, 0.8),
+        plot.title = element_text(size = 8))+
+  guides(linetype = guide_legend(title = NULL))
+p4
+
+#Unite plots ----
+p <- plot_grid(p1,p2,p3,p4,
+          ncol = 2,
+          align = "vh",
+          labels = "AUTO")
+p
+
 #Save plot ----
 
-png(file = "outputs/figures/Gelasimus-vomeris_paints.png",
-    res = 300,
-    units = "in",
-    width = 4.5,
-    height = 4)
-
-plotspec(refletancias,
-         legend = T,
-         ylim = c(0,100))
-
-dev.off()
-
-#Naturals
-png(file = "outputs/figures/Gelasimus-vomeris_reflectances.png",
-    res = 300,
-    units = "in",
-    width = 4.5,
-    height = 4)
-
-plotspec(nat, 
-         legend = TRUE, 
-         ylim = c(0, 100),
-         main = expression(italic("Gelasimus vomeris")))
-
-dev.off()
+ggsave(plot = p, 
+       filename = "outputs/figures/apparatus_colors.png",
+       width = 4, 
+       height = 4, 
+       dpi = 300)
 
 # FIM ----

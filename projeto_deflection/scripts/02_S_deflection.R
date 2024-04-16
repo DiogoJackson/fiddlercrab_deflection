8c46791bddefe8756ac0599257cf9dd45a7163d3
Diogo Silva <56855879+Diogojackson@users.noreply.github.com>
2024-04-16 19:45:17 +1000

    update raw data

--- projeto_deflection/scripts/02_S_deflection.R
+++ projeto_deflection/scripts/02_S_deflection.R
@@ -26,3 +26,11 @@ p2 <- ggplot(filter(dat, experiment == "definitive"), aes(x = first_attack)) +
   labs(x = "First attack", y = "Total of attacks") +
   theme_classic()
 p2
+
+p3 <- ggplot(dat, aes(x = first_attack, fill = attack_position)) +
+  geom_bar(width = 0.6, position = position_dodge(preserve = "single" )) +  # Defina a cor de preenchimento aqui
+  geom_text(stat = "count", aes(label = ..count..), position = position_dodge(width = 0.6), vjust = -0.5, size = 3) +  
+  scale_fill_manual(values = c("grey", "grey33", "grey10"))+
+  labs(x = "First attack", y = "Total of attacks") +
+  theme_classic()
+p3

c60d53282bd7bb2e0a5180c0487a7c0371a95b00
Diogo Silva <56855879+Diogojackson@users.noreply.github.com>
2024-04-22 18:31:31 +1000

    update script deflection

--- projeto_deflection/scripts/02_S_deflection.R
+++ projeto_deflection/scripts/02_S_deflection.R
@@ -13,17 +13,17 @@ library(tidyverse)
 dat <- read_excel("data/raw/data_deflection.xlsx")
 
 #Visualizing data  ----
-dat <- dat %>% 
+dat2 <- dat %>% 
   filter(treatment == "A")
 
-p <- ggplot(dat, aes(x = first_attack)) +
+p <- ggplot(dat2, aes(x = first_attack)) +
   geom_bar(width = 0.6, fill = "#40e0d0") +  # Defina a cor de preenchimento aqui
   stat_count(aes(label = ..count..), geom = "text", vjust = -0.5) +  # Adicione rótulos às barras
   labs(x = "First attack", y = "Total of attacks") +
   theme_classic()
 p
 
-p2 <- ggplot(filter(dat, experiment == "definitive"), aes(x = first_attack)) +
+p2 <- ggplot(filter(dat2, experiment == "definitive"), aes(x = first_attack)) +
   geom_bar(width = 0.6, fill = "#40e0d0") +  # Defina a cor de preenchimento aqui
   stat_count(aes(label = ..count..), geom = "text", vjust = -0.5) +  # Adicione rótulos às barras
   labs(x = "First attack", y = "Total of attacks") +
@@ -35,5 +35,14 @@ p3 <- ggplot(dat, aes(x = first_attack, fill = attack_position)) +
   geom_text(stat = "count", aes(label = ..count..), position = position_dodge(width = 0.6), vjust = -0.5, size = 3) +  
   scale_fill_manual(values = c("grey", "grey33", "grey10"))+
   labs(x = "First attack", y = "Total of attacks") +
-  theme_classic()
+  theme_classic()+
+  facet_grid(~treatment)
 p3
+
+p4 <- ggplot(dat, aes(x = first_attack)) +
+  geom_bar(width = 0.6, fill = "grey") +  # Defina a cor de preenchimento aqui
+  stat_count(aes(label = ..count..), geom = "text", vjust = -0.5) +  # Adicione rótulos às barras
+  labs(x = "First attack", y = "Total of attacks") +
+  theme_classic()+
+  facet_grid(~treatment)
+p4
